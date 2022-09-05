32f2938afe04d96dadc23dbe6eb00df59481465d
Alexander Frolovskii <a.v.frolovskii@gmail.com>
2022-09-05 23:34:04 +0300

    created array

--- js/event-array.js
+++ js/event-array.js
@@ -1,10 +1,66 @@
-let championshipJunior = [
-    {dataStart: '13.09.2022', dataEnd: '18.09.2022'},
-    {place: 'г. Анапа'},
-    {stands: 8},
-    {program: {m10:distance_3m, g10:distance_3m}}
-        
-]
+let championshipJunior = [{
+    dataStart: '13.09.2022', 
+    dataEnd: '18.09.2022',
+    place: 'г. Анапа',
+    stands: 8,
+    program: [
+        {ageGroup: {m10: 'мальчики 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}}, 
+        {ageGroup: {g10: 'девочки 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}}, 
+        {ageGroup: {m12: 'мальчики 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м', distance_4m: 'дистанция 4 м'}}, 
+        {ageGroup: {g12: 'девочки 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м', distance_4m: 'дистанция 4 м'}}, 
+        {ageGroup: {m14: 'юноши 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м', distance_4m: 'дистанция 4 м', distance_5m: 'дистанция 5 м'}},
+        {ageGroup: {g14: 'девушки 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м', distance_4m: 'дистанция 4 м', distance_5m: 'дистанция 5 м'}},
+        {ageGroup: {m16: 'юноши 16-17 лет'}, discipline: {distance_5m: 'дистанция 5 м', distance_6m: 'дистанция 6 м', distance_7m: 'дистанция 7 м'}},
+        {ageGroup: {g16: 'девушки 16-17 лет'}, discipline: {distance_3m: 'дистанция 3 м', distance_4m: 'дистанция 4 м', distance_5m: 'дистанция 5 м'}}
+    ],
+    schedule: [
+        {data: '14.09.2022', program: [
+            {ageGroup: {m10: 'мальчики 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}}, 
+            {ageGroup: {g10: 'девочки 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}}, 
+            {ageGroup: {m12: 'мальчики 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}}, 
+            {ageGroup: {g12: 'девочки 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}}, 
+            {ageGroup: {m14: 'юноши 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {g14: 'девушки 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {m16: 'юноши 16-17 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {g16: 'девушки 16-17 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {m14: 'юноши 14-15 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {g14: 'девушки 14-15 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {m16: 'юноши 16-17 лет'}, discipline: {distance_7m: 'дистанция 7 м'}, stage: {preliminary: 'предварительный этап'}},
+            {ageGroup: {g16: 'девушки 16-17 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {preliminary: 'предварительный этап'}}
+        ]},
+        {data: '15.09.2022', program: [
+            {ageGroup: {m10: 'мальчики 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}}, 
+            {ageGroup: {g10: 'девочки 10-11 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}}, 
+            {ageGroup: {m12: 'мальчики 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}}, 
+            {ageGroup: {g12: 'девочки 12-13 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}}, 
+            {ageGroup: {m14: 'юноши 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {g14: 'девушки 14-15 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {m16: 'юноши 16-17 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {g16: 'девушки 16-17 лет'}, discipline: {distance_3m: 'дистанция 3 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {m14: 'юноши 14-15 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {g14: 'девушки 14-15 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {m16: 'юноши 16-17 лет'}, discipline: {distance_7m: 'дистанция 7 м'}, stage: {final: 'финальный этап'}},
+            {ageGroup: {g16: 'девушки 16-17 лет'}, discipline: {distance_5m: 'дистанция 5 м'}, stage: {final: 'финальный этап'}}
+        ]}
+    ],
+    referee: [
+        {position: {chiefReferee: 'Спиркин Владимир Семенович'}, category: {RC: 'ВК'}},
+        {position: {deputyChiefReferee: 'Иванов Владимир Викторович'}, category: {RC: 'ВК'}}, 
+        {position: {chiefSecretary: 'Пынзарь Сергей Сергеевич'}, category: {RC: 'ВК'}}, 
+        {position: {deputyChiefSecretary: 'Гниломедов Руслан Сергеевич'}, category: {RC: 'ВК'}}, 
+        {position: {seniorRefereeThrowingSector: 'Мякишев Владимир Анатольевич'}, category: {SC: '2К'}}, 
+        {position: {sportsReferee: 'Быков Сергей Владимирович'}, category: {FC: '1К'}}, 
+        {position: {sportsReferee: 'Сикаев Александр Магомедович'}, category: {TC: '3К'}}, 
+        {position: {sportsReferee: 'Булычев Роман Юрьевич'}, category: {SC: '2К'}}, 
+        {position: {sportsReferee: 'Большов Игорь Михайлович'}, category: {TC: '3К'}}
+    ],
+    athletes: [
+        {id: 474774983992222, ageGroup: {m10: 'мальчики 10-11 лет'}, name: 'Сидоров Павел Олегович', birthday: '31.10.2011', region: 'Рязанская область', sportsCategory: {WSC: "б/р"}, discipline: {distance_3m: [
+            
+        ]}}
+    ]
+   
+}]
 
 let ageGroup = {
     m10: 'мальчики 10-11 лет', 
